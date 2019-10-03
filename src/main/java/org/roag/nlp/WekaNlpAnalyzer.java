1cfc4c281cbaf8512ec8250b801801510dc3b313
eurohlam <eurohlam@gmail.com>
2019-10-03 23:05:47 +1300

    weka example and dependencies, but still not working

--- src/main/java/org/roag/nlp/WekaNlpAnalyzer.java
+++ src/main/java/org/roag/nlp/WekaNlpAnalyzer.java
@@ -1,4 +1,51 @@
 package org.roag.nlp;
 
+import weka.classifiers.evaluation.Evaluation;
+import weka.classifiers.functions.Dl4jMlpClassifier;
+import weka.core.Instances;
+import weka.dl4j.NeuralNetConfiguration;
+import weka.dl4j.activations.ActivationSoftmax;
+import weka.dl4j.layers.Layer;
+import weka.dl4j.layers.OutputLayer;
+import weka.dl4j.lossfunctions.LossMCXENT;
+import weka.dl4j.updater.Adam;
+
+import java.io.FileReader;
+import java.util.Random;
+
 public class WekaNlpAnalyzer {
+
+    public static void main(String[] args) {
+        try {
+// Create a new Multi-Layer-Perceptron classifier
+            Dl4jMlpClassifier clf = new Dl4jMlpClassifier();
+// Set a seed for reproducable results
+            clf.setSeed(1);
+
+// Load the iris dataset and set its class index
+            Instances data = new Instances(new FileReader("/Users/eurohlam/IdeaProjects/wekaDeeplearning4j/datasets/nominal/iris.arff"));
+            data.setClassIndex(data.numAttributes() - 1);
+
+// Define the output layer
+            OutputLayer outputLayer = new OutputLayer();
+            outputLayer.setActivationFunction(new ActivationSoftmax());
+            outputLayer.setLossFn(new LossMCXENT());
+
+            NeuralNetConfiguration nnc = new NeuralNetConfiguration();
+            nnc.setUpdater(new Adam());
+
+// Add the layers to the classifier
+            clf.setLayers(new Layer[]{outputLayer});
+            clf.setNeuralNetConfiguration(nnc);
+
+// Evaluate the network
+            Evaluation eval = new Evaluation(data);
+            int numFolds = 10;
+            eval.crossValidateModel(clf, data, numFolds, new Random(1));
+
+            System.out.println(eval.toSummaryString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
